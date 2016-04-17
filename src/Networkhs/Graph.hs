ee9b1b06eaadedaa61d01dc7c8ac0da45e9b454b
starcolon <tao@starcolon.com>
2016-04-17 20:38:03 +0700

    {GRAPH} Restructure - store both linksFrom and LinksTo in graph

--- src/Networkhs/Graph.hs
+++ src/Networkhs/Graph.hs
@@ -14,8 +14,11 @@ data Node a = Node
 
 data Graph a = Graph
 	{
-	  nodes    :: [Node a]
-	, links    :: M.Map String Double
+	  nodes     :: [Node a]
+	, linksTo   :: M.Map String [(String,Double)]
+	, linksFrom :: M.Map String [(String,Double)]
+	-- NOTE: Links are stored as redundancy in both directions
+	-- just in order to speed up look up time in both ways.
 	}
 	deriving (Show)
 
@@ -23,12 +26,35 @@ data Graph a = Graph
 ----------------------------
 
 newGraph :: [Node a] -> Graph a
-newGraph ns = Graph { nodes = ns, links = M.fromList []}
-
-linkKey :: String -> String -> String
-linkKey a b = unlines [a,b]
+newGraph ns = Graph { 
+	nodes = ns, linksFrom = M.fromList [], linksTo = M.fromList []
+	}
 
 addLink :: (String,String,Double) -> Graph a -> Graph a
-addLink (n0,n1,w) g = let lmap = links g 
-	in g { links = M.insert (linkKey n0 n1) w lmap } -- This will always update existing link
+addLink (n0,n1,w) g = let g' = __addLinkTo (n0,n1,w) g
+	in __addLinkFrom (n0,n1,w) g'
 
+__addLinkTo :: (String,String,Double) -> Graph a -> Graph a
+__addLinkTo (n0,n1,w) g = let lnks = linksTo g in
+	case M.lookup n1 lnks of
+		Nothing -> g { linksTo = M.insert n1 [(n0,w)] lnks }
+		Just array -> g { linksTo = M.insert n1 array'' lnks }
+			where {
+				array'  = [(a,w) | (a,w) -> array, a != n0];
+				array'' = array' ++ [(n0,w)]
+			}
+
+__addLinkFrom :: (String,String,Double) -> Graph a -> Graph a
+__addLinkFrom (n0,n1,w) g = let lnks = linksFrom g in
+	case M.lookup n0 lnks of
+		Nothing -> g { linksFrom = M.insert n0 [(n1,w)] lnks }
+		Just array -> g { linksFrom = M.insert n0 array'' lnks }
+			where{
+				array'  = [(b,w) | (b,w) -> array, b != n1],
+				array'' = array' ++ [(n1,w)]
+			}
+	
+
+addBiLink :: (String,String,Double) -> Graph a -> Graph a
+addBiLink (n0,n1,w) g = let g' = addLink (n0,n1,w) g
+	in addLink (n1,n0,w) g'
--- test/Networkhs/GraphSpec.hs
+++ test/Networkhs/GraphSpec.hs
@@ -2,7 +2,6 @@ module Networkhs.GraphSpec (main,spec) where
 
 import Test.Hspec
 import Test.QuickCheck
---import Control.Exception (evaluate)
 import qualified Networkhs.Graph as G
 
 main :: IO ()
@@ -15,3 +14,11 @@ spec = do
 			length G.nodes g `shouldBe` 0
 				where g = G.newGraph []
 
+		it "should create a graph with nodes" $ do
+			G.nodes g `shouldBe` [a,b,c]
+				where {
+					a = G.Node "brasil" 100;
+					b = G.Node "australia" 200;
+					c = G.Node "japan" 300;
+					g = G.newGraph [a,b,c]
+				}
