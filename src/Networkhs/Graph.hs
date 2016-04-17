6ffc5d680018bb399fc3fdb27970d50931103d4c
starcolon <tao@starcolon.com>
2016-04-18 00:39:02 +0700

    Fix typos

--- src/Networkhs/Graph.hs
+++ src/Networkhs/Graph.hs
@@ -40,7 +40,7 @@ __addLinkTo (n0,n1,w) g = let lnks = linksTo g in
 		Nothing -> g { linksTo = M.insert n1 [(n0,w)] lnks }
 		Just array -> g { linksTo = M.insert n1 array'' lnks }
 			where {
-				array'  = [(a,w) | (a,w) -> array, a != n0];
+				array'  = [(a,w) | (a,w) <- array, a /= n0];
 				array'' = array' ++ [(n0,w)]
 			}
 
@@ -50,7 +50,7 @@ __addLinkFrom (n0,n1,w) g = let lnks = linksFrom g in
 		Nothing -> g { linksFrom = M.insert n0 [(n1,w)] lnks }
 		Just array -> g { linksFrom = M.insert n0 array'' lnks }
 			where{
-				array'  = [(b,w) | (b,w) -> array, b != n1],
+				array'  = [(b,w) | (b,w) <- array, b /= n1];
 				array'' = array' ++ [(n1,w)]
 			}
 	
