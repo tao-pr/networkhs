754ceb14ce598b9ae3b6e7bffd5c5f9138f0c9b5
starcolon <tao@starcolon.com>
2016-04-30 16:03:35 +0700

    {GRAPH} Implement #edges function - untested

--- src/Networkhs/Graph.hs
+++ src/Networkhs/Graph.hs
@@ -3,7 +3,8 @@ module Networkhs.Graph where
 import Prelude
 import Data.Maybe
 import Data.String
-import Data.Map as M
+import Data.List(sortBy)
+import Data.Map as M hiding (map,foldl)
 
 data Node a = Node
   { key      :: String
@@ -25,19 +26,24 @@ data Route = Route [(String,String)]
 
 ----------------------------
 
+-- | Create a new graph from given nodes
 newGraph :: [Node a] -> Graph a
 newGraph ns = Graph { nodes = ns
                     , linksFrom = M.fromList []
                     , linksTo = M.fromList []
                     }
 
+-- | Add a new node to the graph
 addNode :: (String,a) -> Graph a -> Graph a
 addNode (s,a) g = g { nodes = (nodes g) ++ [Node s a]}
 
+-- | Add a new link to the graph (idempotent function)
+-- or replace an existing link with new weight
 addLink :: Graph a -> (String,String,Double) -> Graph a
 addLink g (n0,n1,w) = let g' = __addLinkTo g (n0,n1,w)
   in __addLinkFrom g' (n0,n1,w)
 
+-- | Add a new directional link to a node (augmented function)
 __addLinkTo ::  Graph a -> (String,String,Double) -> Graph a
 __addLinkTo g (n0,n1,w) = let lnks = linksTo g in
   case M.lookup n1 lnks of
@@ -47,6 +53,8 @@ __addLinkTo g (n0,n1,w) = let lnks = linksTo g in
             ; array'' = array' ++ [(n0,w)]
             }
 
+-- | Add a new directional link from a node to another node
+-- (augmented function)
 __addLinkFrom :: Graph a -> (String,String,Double) -> Graph a
 __addLinkFrom g (n0,n1,w) = let lnks = linksFrom g in
   case M.lookup n0 lnks of
@@ -55,11 +63,13 @@ __addLinkFrom g (n0,n1,w) = let lnks = linksFrom g in
       where { array'  = [(b,w) | (b,w) <- array, b /= n1]
             ; array'' = array' ++ [(n1,w)]
             }
-  
+
+-- | Add a bidirectional link with the same weight
 addBiLink :: Graph a -> (String,String,Double) -> Graph a
 addBiLink g (n0,n1,w) = let g' = addLink g (n0,n1,w)
   in addLink g' (n1,n0,w)
 
+-- | Get the weight of the link from a node to one another
 link :: String -> String -> Graph a -> Maybe Double
 link n0 n1 g = case M.lookup n0 (linksFrom g) of
   Nothing -> Nothing
@@ -67,9 +77,16 @@ link n0 n1 g = case M.lookup n0 (linksFrom g) of
     let ns = [ w | (n,w) <- array, n==n1]
     in if Prelude.null ns then Nothing else Just $ head ns
 
+-- | Get links in both ways from a node to one another
 linkBackAndForth :: String -> String -> Graph a -> (Maybe Double, Maybe Double)
 linkBackAndForth n0 n1 g = (link n0 n1 g, link n1 n0 g)
 
+-- | Comparison function for links
+__orderLink :: (String,String,Double) -> (String,String,Double) -> Ordering
+__orderLink (_,_,w1) (_,_,w2) = compare w1 w2
+
+-- | Create a node route from given path
+-- a path could be a list of node names in sequence
 newRoute :: [String] -> Route
 newRoute ns   = 
   -- Route must consist of at least two blocks of node
@@ -84,6 +101,7 @@ newRoute ns   =
       Route [] -> Route r0
       Route rn' -> Route $ r0 ++ rn'
 
+-- | Calculate total distance to walk a route
 routeDistance :: Route -> Graph a -> Maybe Double
 routeDistance r g = case r of
   Route []     -> Nothing
@@ -100,9 +118,29 @@ routeDistance r g = case r of
         Nothing     -> Nothing
         Just dists' -> Just (dist0' + dists')
 
--- TAOTODO:
---dfvs :: String -> Graph a -> [Route] -> [Route]
+-- | Expand a tuple of link (converted from map)
+--   to a list of link tuples
+__flattenLink :: (String,[(String,Double)]) -> [(String,String,Double)]
+__flattenLink (n0,ts) = map (\(n1,w) -> (n0,n1,w) ) ts
+__flattenLink otherwise = error "Invalid link pattern"
 
+-- | Convert a link map to a plain list
+__linksToList :: M.Map String [(String,Double)] -> [(String,String,Double)]
+__linksToList m = let { ls  = M.toList m 
+                      ; ls' = map __flattenLink ls
+                      }
+                  in foldl (++) [] ls'
+
+-- | Get a list of edges of a graph, ordered by weight (smallest first)
+edges :: Graph a -> [(String,String,Double)]
+edges g = let { lnks  = linksFrom g 
+              ; lls   = __linksToList lnks -- Expand the list of link tuples
+  }
+  in sortBy __orderLink lls
+
+-- | Compute a minimum spanning tree of a graph
+spanTree :: Graph a -> Graph a
+spanTree g = error "TAOTODO: Implement this"
 
 
 
