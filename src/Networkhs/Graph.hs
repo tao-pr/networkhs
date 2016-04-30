be2a383ab1d2ca193421b35df6174731c2ae3d03
starcolon <tao@starcolon.com>
2016-04-30 11:59:29 +0700

    Apply Haskell conventions to entire project code

--- src/Networkhs/Graph.hs
+++ src/Networkhs/Graph.hs
@@ -6,21 +6,19 @@ import Data.String
 import Data.Map as M
 
 data Node a = Node
-	{
-	  key      :: String
-	, content  :: a
-	}
-	deriving (Show)
+  { key      :: String
+  , content  :: a
+  }
+  deriving (Show)
 
 data Graph a = Graph
-	{
-	  nodes     :: [Node a]
-	, linksTo   :: M.Map String [(String,Double)]
-	, linksFrom :: M.Map String [(String,Double)]
-	-- NOTE: Links are stored as redundancy in both directions
-	-- just in order to speed up look up time in both ways.
-	}
-	deriving (Show)
+  { nodes     :: [Node a]
+  , linksTo   :: M.Map String [(String,Double)]
+  , linksFrom :: M.Map String [(String,Double)]
+  -- NOTE: Links are stored as redundancy in both directions
+  -- just in order to speed up look up time in both ways.
+  }
+  deriving (Show)
 
 data Route = Route [(String,String)]
 
@@ -28,82 +26,83 @@ data Route = Route [(String,String)]
 ----------------------------
 
 newGraph :: [Node a] -> Graph a
-newGraph ns = Graph { 
-	nodes = ns, linksFrom = M.fromList [], linksTo = M.fromList []
-	}
+newGraph ns = Graph { nodes = ns
+                    , linksFrom = M.fromList []
+                    , linksTo = M.fromList []
+                    }
 
 addNode :: (String,a) -> Graph a -> Graph a
 addNode (s,a) g = g { nodes = (nodes g) ++ [Node s a]}
 
 addLink :: Graph a -> (String,String,Double) -> Graph a
 addLink g (n0,n1,w) = let g' = __addLinkTo g (n0,n1,w)
-	in __addLinkFrom g' (n0,n1,w)
+  in __addLinkFrom g' (n0,n1,w)
 
 __addLinkTo ::  Graph a -> (String,String,Double) -> Graph a
 __addLinkTo g (n0,n1,w) = let lnks = linksTo g in
-	case M.lookup n1 lnks of
-		Nothing -> g { linksTo = M.insert n1 [(n0,w)] lnks }
-		Just array -> g { linksTo = M.insert n1 array'' lnks }
-			where {
-				array'  = [(a,w) | (a,w) <- array, a /= n0];
-				array'' = array' ++ [(n0,w)]
-			}
+  case M.lookup n1 lnks of
+    Nothing -> g { linksTo = M.insert n1 [(n0,w)] lnks }
+    Just array -> g { linksTo = M.insert n1 array'' lnks }
+      where { array'  = [(a,w) | (a,w) <- array, a /= n0]
+            ; array'' = array' ++ [(n0,w)]
+            }
 
 __addLinkFrom :: Graph a -> (String,String,Double) -> Graph a
 __addLinkFrom g (n0,n1,w) = let lnks = linksFrom g in
-	case M.lookup n0 lnks of
-		Nothing -> g { linksFrom = M.insert n0 [(n1,w)] lnks }
-		Just array -> g { linksFrom = M.insert n0 array'' lnks }
-			where{
-				array'  = [(b,w) | (b,w) <- array, b /= n1];
-				array'' = array' ++ [(n1,w)]
-			}
-	
-
+  case M.lookup n0 lnks of
+    Nothing -> g { linksFrom = M.insert n0 [(n1,w)] lnks }
+    Just array -> g { linksFrom = M.insert n0 array'' lnks }
+      where { array'  = [(b,w) | (b,w) <- array, b /= n1]
+            ; array'' = array' ++ [(n1,w)]
+            }
+  
 addBiLink :: Graph a -> (String,String,Double) -> Graph a
 addBiLink g (n0,n1,w) = let g' = addLink g (n0,n1,w)
-	in addLink g' (n1,n0,w)
+  in addLink g' (n1,n0,w)
 
 link :: String -> String -> Graph a -> Maybe Double
 link n0 n1 g = case M.lookup n0 (linksFrom g) of
-	Nothing -> Nothing
-	Just array -> 
-		let ns = [ w | (n,w) <- array, n==n1]
-		in if Prelude.null ns then Nothing else Just $ head ns
+  Nothing -> Nothing
+  Just array -> 
+    let ns = [ w | (n,w) <- array, n==n1]
+    in if Prelude.null ns then Nothing else Just $ head ns
 
 linkBackAndForth :: String -> String -> Graph a -> (Maybe Double, Maybe Double)
 linkBackAndForth n0 n1 g = (link n0 n1 g, link n1 n0 g)
 
 newRoute :: [String] -> Route
 newRoute ns   = 
-	-- Route must consist of at least two blocks of node
-	if length ns < 2 then Route []
-	else
-		let { 
-			n0 = head ns; 
-			n1 = (head . tail) ns;
-			r0 = [(n0,n1)];
-			rn = newRoute $ tail ns;
-		}
-		in case rn of 
-			Route [] -> Route r0
-			Route rn' -> Route $ r0 ++ rn'
+  -- Route must consist of at least two blocks of node
+  if length ns < 2 then Route []
+  else
+    let { n0 = head ns
+        ; n1 = (head . tail) ns
+        ; r0 = [(n0,n1)]
+        ; rn = newRoute $ tail ns
+    }
+    in case rn of 
+      Route [] -> Route r0
+      Route rn' -> Route $ r0 ++ rn'
 
 routeDistance :: Route -> Graph a -> Maybe Double
 routeDistance r g = case r of
-	Route []     -> Nothing
-	Route (q:[]) -> case q of
-		(n0,n1)    -> link n0 n1 g 
-		otherwise  -> Nothing -- Safeguard
-	Route (q:qs) -> let { 
-		dist0 = routeDistance (Route [q]) g;
-		dists = routeDistance (Route qs) g;
-		}
-		in case dist0 of
-			Nothing     -> Nothing
-			Just dist0' -> case dists of
-				Nothing     -> Nothing
-				Just dists' -> Just (dist0' + dists')
+  Route []     -> Nothing
+  Route (q:[]) -> case q of
+    (n0,n1)    -> link n0 n1 g 
+    otherwise  -> Nothing -- Safeguard
+  Route (q:qs) -> let { 
+    dist0 = routeDistance (Route [q]) g;
+    dists = routeDistance (Route qs) g;
+    }
+    in case dist0 of
+      Nothing     -> Nothing
+      Just dist0' -> case dists of
+        Nothing     -> Nothing
+        Just dists' -> Just (dist0' + dists')
+
+-- TAOTODO:
+--dfvs :: String -> Graph a -> [Route] -> [Route]
+
 
 
 
--- test/Networkhs/GraphSpec.hs
+++ test/Networkhs/GraphSpec.hs
@@ -1,8 +1,8 @@
 module Networkhs.GraphSpec (main,spec) where
 
 import Test.Hspec
-import Test.QuickCheck
-import Data.Maybe
+import Test.QuickCheck()
+import Data.Maybe()
 import qualified Networkhs.Graph as G
 
 main :: IO ()
@@ -10,95 +10,87 @@ main = hspec spec
 
 -- Simple graph without any links
 graph1 :: G.Graph [Char]
-graph1 = let{
-		xs = ["beijing","osaka","bangkok","moscow"];
-		ns = map (\n -> G.Node n "city") xs;
-	}
-	in G.newGraph ns
+graph1 = 
+  let { xs = ["beijing","osaka","bangkok","moscow"]
+      ; ns = map (\n -> G.Node n "city") xs
+      }
+  in G.newGraph ns
 
 -- Simple graph with some bidirectional links
 graph2 :: G.Graph [Char]
-graph2 = let links = [
-			("beijing","osaka",3),
-			("beijing","bangkok",8),
-			--("beijing","moscow",10),
-			("moscow","bangkok",15),
-			("osaka","moscow",10),
-			("osaka","bangkok",12)
-		]
-		in foldl (G.addBiLink) graph1 links;
+graph2 = 
+  let links = [ ("beijing","osaka",3)
+              , ("beijing","bangkok",8)
+              , ("moscow","bangkok",15)
+              , ("osaka","moscow",10)
+              , ("osaka","bangkok",12) ]
+  in foldl (G.addBiLink) graph1 links
 
 spec :: Spec
 spec = do
-	describe "Fundamental Graph structure tests" $ do
-		it "should create an empty graph" $ do
-			let {g = G.newGraph []; len = length $ G.nodes g }
-				in len `shouldBe` 0
+  describe "Fundamental Graph structure tests" $ do
+    it "should create an empty graph" $ do
+      let {g = G.newGraph []; len = length $ G.nodes g }
+        in len `shouldBe` 0
 
-		it "should create a graph with nodes" $ do
-			let {
-				a = G.Node "brasil" Nothing;
-				b = G.Node "australia" Nothing;
-				c = G.Node "japan" Nothing;
-				g = G.newGraph [a,b,c]
-			}
-				in map (G.key) (G.nodes g) `shouldBe` ["brasil","australia","japan"]
+    it "should create a graph with nodes" $ do
+      let { a = G.Node "brasil" Nothing
+          ; b = G.Node "australia" Nothing
+          ; c = G.Node "japan" Nothing
+          ; g = G.newGraph [a,b,c]
+      }
+        in map (G.key) (G.nodes g) `shouldBe` ["brasil","australia","japan"]
 
-		it "should add new node to a graph" $ do
-			let {
-				g = G.addNode ("singapore","") graph1;
-			}
-				in map (G.key) (G.nodes g) `shouldBe` ["beijing","osaka","bangkok","moscow","singapore"]
+    it "should add new node to a graph" $ do
+      let g = G.addNode ("singapore","") graph1
+        in map (G.key) (G.nodes g) `shouldBe` ["beijing","osaka","bangkok","moscow","singapore"]
 
-		it "should add a link" $ do
-			let {
-				g  = G.addLink graph1 ("beijing","osaka",5.0);
-				g' = G.addLink g ("osaka","bangkok",12.5);
-				l1 = G.link "beijing" "osaka" g';
-				l2 = G.link "osaka" "bangkok" g';
-				l3 = G.link "osaka" "moscow" g';
-			}
-				in [l1,l2,l3] `shouldBe` [Just 5.0, Just 12.5, Nothing]
+    it "should add a link" $ do
+      let { g  = G.addLink graph1 ("beijing","osaka",5.0)
+          ; g' = G.addLink g ("osaka","bangkok",12.5)
+          ; l1 = G.link "beijing" "osaka" g'
+          ; l2 = G.link "osaka" "bangkok" g'
+          ; l3 = G.link "osaka" "moscow" g'
+      }
+        in [l1,l2,l3] `shouldBe` [Just 5.0, Just 12.5, Nothing]
 
-		it "bidirectional link vs oneway link" $ do
-			let {
-				g  = G.addLink graph1 ("beijing","osaka",1.1);
-				g' = G.addBiLink g ("bangkok","beijing",2.2);
-				l1 = G.link "beijing" "osaka" g';
-				l2 = G.link "osaka" "beijing" g';
-				l3 = G.link "bangkok" "beijing" g';
-				l4 = G.link "beijing" "bangkok" g';
-			}
-				in [l1,l2,l3,l4] `shouldBe` [Just 1.1, Nothing, Just 2.2, Just 2.2]
+    it "bidirectional link vs oneway link" $ do
+      let { g  = G.addLink graph1 ("beijing","osaka",1.1)
+          ; g' = G.addBiLink g ("bangkok","beijing",2.2)
+          ; l1 = G.link "beijing" "osaka" g'
+          ; l2 = G.link "osaka" "beijing" g'
+          ; l3 = G.link "bangkok" "beijing" g'
+          ; l4 = G.link "beijing" "bangkok" g'
+      }
+        in [l1,l2,l3,l4] `shouldBe` [Just 1.1, Nothing, Just 2.2, Just 2.2]
 
-		it "should get links back and forth" $ do
-			let {
-				g  = G.addLink graph1 ("beijing","osaka",1.1);
-				g' = G.addBiLink g ("bangkok","beijing",2.2);
-				l1 = G.linkBackAndForth "beijing" "osaka" g';
-				l2 = G.linkBackAndForth "beijing" "bangkok" g';
-			}
-				in [l1,l2] `shouldBe` [(Just 1.1,Nothing),(Just 2.2, Just 2.2)]
+    it "should get links back and forth" $ do
+      let { g  = G.addLink graph1 ("beijing","osaka",1.1)
+          ; g' = G.addBiLink g ("bangkok","beijing",2.2)
+          ; l1 = G.linkBackAndForth "beijing" "osaka" g'
+          ; l2 = G.linkBackAndForth "beijing" "bangkok" g'
+      }
+        in [l1,l2] `shouldBe` [(Just 1.1,Nothing),(Just 2.2, Just 2.2)]
 
-	describe "Fundamental route structure tests" $ do
-		it "should create a new route" $ do
-			case G.newRoute ["a","b","c","d"] of
-				G.Route [] -> error "Route should not be empty"
-				G.Route r -> r `shouldBe` [("a","b"),("b","c"),("c","d")]
+  describe "Fundamental route structure tests" $ do
+    it "should create a new route" $ do
+      case G.newRoute ["a","b","c","d"] of
+        G.Route [] -> error "Route should not be empty"
+        G.Route r -> r `shouldBe` [("a","b"),("b","c"),("c","d")]
 
-		it "should compute distance of a single hop" $ do
-			let r = G.Route [("beijing","osaka")]
-				in G.routeDistance r graph2 `shouldBe` Just 3
+    it "should compute distance of a single hop" $ do
+      let r = G.Route [("beijing","osaka")]
+        in G.routeDistance r graph2 `shouldBe` Just 3
 
 
-		it "should compute total distance" $ do
-			let r = G.Route [("beijing","osaka"),("osaka","bangkok"),("bangkok","moscow")]
-				in G.routeDistance r graph2 `shouldBe` Just 30
+    it "should compute total distance" $ do
+      let r = G.Route [("beijing","osaka"),("osaka","bangkok"),("bangkok","moscow")]
+        in G.routeDistance r graph2 `shouldBe` Just 30
 
-		it "should fail to compute distance of unknown node" $ do
-			let r = G.Route [("bangkok","osaka"),("osaka","shanghai")]
-				in G.routeDistance r graph2 `shouldSatisfy` null
+    it "should fail to compute distance of unknown node" $ do
+      let r = G.Route [("bangkok","osaka"),("osaka","shanghai")]
+        in G.routeDistance r graph2 `shouldSatisfy` null
 
-		it "should fail to compute distance of unconnected route" $ do
-			let r = G.newRoute ["beijing","osaka","bangkok","moscow","beijing"]
-				in G.routeDistance r graph2 `shouldSatisfy` null
+    it "should fail to compute distance of unconnected route" $ do
+      let r = G.newRoute ["beijing","osaka","bangkok","moscow","beijing"]
+        in G.routeDistance r graph2 `shouldSatisfy` null
